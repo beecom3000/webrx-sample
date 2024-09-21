04efc098469ee40e04fb92d3ee4e3fa91030ac96
beecom <bee.nakhakes@gmail.com>
2024-09-21 12:33:54 +1000

    Updates

--- assets/WebXRView-BjMi9_o8.js
+++ assets/WebXRView-BjMi9_o8.js
@@ -1,4 +1,4 @@
-import{d as mo,r as ur,b as _o,e as go,o as vo,c as xo,a as Cn,f as Mo,w as Fs,g as So,t as Eo,_ as yo}from"./index-Yw8bZMf2.js";/**
+import{d as mo,r as ur,b as _o,e as go,o as vo,c as xo,a as Cn,f as Mo,w as Fs,g as So,t as Eo,_ as yo}from"./index-QgOdczPV.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
