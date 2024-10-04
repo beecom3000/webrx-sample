b777b32d6a321cb6eb359421e53c3b9f8b3c425f
beecom <bee.nakhakes@gmail.com>
2024-10-04 17:21:37 +1000

    add babylon example

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,10 @@
   </modal-dialog>
 
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      Web XR
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
